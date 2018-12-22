5e7839d4c58443c41eb701bfacead70bf69503c5
Maxime Soulé <btik-git@scoubidou.com>
2018-12-22 22:19:29 +0100

    Do not get stuck anymore when devices are not connected to vitodata
    Signed-off-by: Maxime Soulé <btik-git@scoubidou.com>

--- main.go
+++ main.go
@@ -58,6 +58,101 @@ func getAttrValue(vdev *vitotrol.Device, attrID vitotrol.AttrID) (value interfac
 var customAttr = regexp.MustCompile(
 	`^([a-zA-Z0-9]+)[-_]0x([a-fA-F0-9]{1,4})\z`)
 
+func handleDevices(conf *Config, pVitotrol *vitotrol.Session, influx client.Client) bool {
+	atLeastOneOK := false
+	for _, vdev := range pVitotrol.Devices {
+		start := time.Now()
+
+		if !vdev.IsConnected {
+			continue
+		}
+
+		// Check if this device has a configuration
+		cdev := conf.GetConfigDevice(vdev.DeviceName, vdev.LocationName)
+		if cdev == nil {
+			continue
+		}
+
+		ch, err := vdev.RefreshDataWait(pVitotrol, cdev.attrs)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "RefreshData error: %s\n", err)
+			continue
+		}
+
+		atLeastOneOK = true
+
+		// Create a new point batch
+		bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+			Database:         cdev.Database,
+			Precision:        cdev.Precision,
+			RetentionPolicy:  cdev.RetentionPolicy,
+			WriteConsistency: cdev.WriteConsistency,
+		})
+		if err != nil {
+			fmt.Printf("influx.NewBatchPoints failed: %s\n", err)
+			os.Exit(1)
+		}
+
+		if err = <-ch; err != nil {
+			fmt.Fprintf(os.Stderr, "RefreshData failed: %s\n", err)
+			continue
+		}
+
+		now := time.Now()
+
+		err = vdev.GetData(pVitotrol, cdev.attrs)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "GetData error: %s\n", err)
+			continue
+		}
+
+		fields := map[string]interface{}{}
+
+		for _, attrID := range cdev.attrs {
+			fields[vitotrol.AttributesRef[attrID].Name] =
+				getAttrValue(&vdev, attrID)
+		}
+
+		// Computed attrs
+		for _, fieldName := range cdev.computedFields {
+			fields[fieldName] = computedAttrs[fieldName].Compute(&vdev)
+		}
+
+		// If no tags are configured, use defaults
+		tags := cdev.Tags
+		if len(tags) == 0 {
+			tags = map[string]string{
+				"device":   vdev.DeviceName,
+				"location": vdev.LocationName,
+			}
+		}
+
+		point, err := client.NewPoint(cdev.Measurement, tags, fields, now)
+		if err != nil {
+			fmt.Printf("influx.NewPoint failed: %s\n", err)
+			os.Exit(1)
+		}
+
+		bp.AddPoint(point)
+
+		fmt.Printf("%s %s → writing batch...\n",
+			now.Format(time.RFC3339),
+			now.Sub(start).Truncate(time.Millisecond),
+		)
+
+		// Write the batch
+		err = influx.Write(bp)
+		if err != nil {
+			fmt.Printf("influx.Write failed: %s\n", err)
+			os.Exit(1)
+		}
+
+		time.Sleep(time.Duration(conf.Vitotrol.Frequency) * time.Second)
+	}
+
+	return atLeastOneOK
+}
+
 func main() {
 	configFile := flag.String("config", "", "config file")
 
@@ -138,92 +233,9 @@ newVitotrol:
 		pVitotrol := VitotrolInit(&conf.Vitotrol)
 
 		for {
-			start := time.Now()
-
-			for _, vdev := range pVitotrol.Devices {
-				if !vdev.IsConnected {
-					continue
-				}
-
-				// Check if this device has a configuration
-				cdev := conf.GetConfigDevice(vdev.DeviceName, vdev.LocationName)
-				if cdev == nil {
-					continue
-				}
-
-				ch, err := vdev.RefreshDataWait(pVitotrol, cdev.attrs)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "RefreshData error: %s\n", err)
-					continue newVitotrol
-				}
-
-				// Create a new point batch
-				bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-					Database:         cdev.Database,
-					Precision:        cdev.Precision,
-					RetentionPolicy:  cdev.RetentionPolicy,
-					WriteConsistency: cdev.WriteConsistency,
-				})
-				if err != nil {
-					fmt.Printf("influx.NewBatchPoints failed: %s\n", err)
-					os.Exit(1)
-				}
-
-				if err = <-ch; err != nil {
-					fmt.Fprintf(os.Stderr, "RefreshData failed: %s\n", err)
-					continue newVitotrol
-				}
-
-				now := time.Now()
-
-				err = vdev.GetData(pVitotrol, cdev.attrs)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "GetData error: %s\n", err)
-					continue newVitotrol
-				}
-
-				fields := map[string]interface{}{}
-
-				for _, attrID := range cdev.attrs {
-					fields[vitotrol.AttributesRef[attrID].Name] =
-						getAttrValue(&vdev, attrID)
-				}
-
-				// Computed attrs
-				for _, fieldName := range cdev.computedFields {
-					fields[fieldName] = computedAttrs[fieldName].Compute(&vdev)
-				}
-
-				// If no tags are configured, use defaults
-				tags := cdev.Tags
-				if len(tags) == 0 {
-					tags = map[string]string{
-						"device":   vdev.DeviceName,
-						"location": vdev.LocationName,
-					}
-				}
-
-				point, err := client.NewPoint(cdev.Measurement, tags, fields, now)
-				if err != nil {
-					fmt.Printf("influx.NewPoint failed: %s\n", err)
-					os.Exit(1)
-				}
-
-				bp.AddPoint(point)
-
-				fmt.Printf("%s %s → writing batch...\n",
-					now.Format(time.RFC3339),
-					now.Sub(start).Truncate(time.Millisecond),
-				)
-
-				// Write the batch
-				err = influx.Write(bp)
-				if err != nil {
-					fmt.Printf("influx.Write failed: %s\n", err)
-					os.Exit(1)
-				}
-
-				time.Sleep(time.Duration(conf.Vitotrol.Frequency) * time.Second)
+			if !handleDevices(conf, pVitotrol, influx) {
+				time.Sleep(time.Duration(30 * time.Second))
+				continue newVitotrol
 			}
 		}
 	}
